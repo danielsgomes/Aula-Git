9d4ae26eb2e0c32b9b90a0f90cc59859507f273a
danielsgomes <danielilustrador@gmail.com>
2024-05-04 11:52:32 -0300

    Primeira mudança

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,60 @@
+package org.example
+
+fun main() {
+
+
+    println("hello, world")
+}
+
+/*
+
+Git: É um padrão que permite uma pasta comum no PC que permite criar e editar no mesmo
+arquivo várias pessoas ao
+
+ Repositório -> Pasta
+
+ Branch -> cópia do código (versão)
+
+ Comando: Git version
+
+ Isntala e depois precisa ser conectado na internet
+
+ GitHub: É a nuvem da Git.
+
+ Git é o sistema que usamos para puxar, enviar e editar.
+
+ GitHub - http//github.com
+
+ Configurar o Git no Github:
+
+
+ "PS C:\Users\Developer\IdeaProjects\Aula-git> git version
+git version 2.45.0.windows.1
+PS C:\Users\Developer\IdeaProjects\Aula-git> "
+
+Acesso: danielilustrador@gmail.com
+Username: danielsgomes
+Senha: GitDaniHub24
+
+Commit - uma mensagem de descrição  commit -m "(descritive message)" — comentário ou título.
+Push - envia para o GitHub. - Upload — git push
+Pull - baixa o computador - Download — git pull
+Add adicionar coisas - git add .
+git status - saber se mandei tudo que precisava.qq':
+::qI
+
+
+ Ler como funciona essa documentação.
+
+ Baixar e salvear
+
+
+
+
+
+
+
+
+
+
+ */
